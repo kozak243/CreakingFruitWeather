200346f1b505988dad7e8fdbe54c65440cd486c3
andrew <andrew.lyle@tshirtgang.com>
2019-12-13 13:44:40 -0500

    first commit
    builds an icon with local weather in your Windows system tray

---
+++ weather.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import threading
+from infi.systray import SysTrayIcon
+from PIL import Image, ImageDraw,ImageFont
+import time
+#windows tray icon
+# @see https://github.com/Infinidat/infi.systray
+#open weather API
+# @see https://openweathermap.org/api
+# api-endpoint 
+#variables
+city = "{CITY HERE}"
+country = "US" #two digit code
+units = "metric" #testing or imperial
+
+#api key
+weatherapi_key = "{API_KEY_HERE}"
+URL = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units="+units+"&appid="+weatherapi_key
+
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = ""
+
+HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+def checkWeather():
+    #threading.Timer(60.0, checkWeather).start()
+    # sending get request and saving the response as response object 
+    r = requests.post(url = URL, json = PARAMS, headers = HEADERS) 
+
+    # extracting data in json format 
+    data = r.json() 
+
+    # extracting latitude, longitude and formatted address  
+    # of the first matching location    
+    return int(data['main']['temp'])
+
+#draw icon
+image= "pil_text.ico"
+n=1
+while True:
+    #check weather
+    temp = checkWeather()
+    # create image
+    img = Image.new('RGBA', (50, 50), color = (255, 255, 255, 0))  # color background =  white  with transparency
+    d = ImageDraw.Draw(img)
+
+    #add text to the image
+    font_type  = ImageFont.truetype("arial.ttf", 40)
+    d.text((5,5), str(temp)+"°", fill=(255,255,255), font = font_type)
+
+    img.save(image)
+
+    # display image in systray 
+    if n==1:
+        systray = SysTrayIcon(image, city+" weather")
+        systray.start()
+    else:
+        systray.update(icon=image)
+    time.sleep(30)
+    n+=1
+systray.shutdown()
+
