baa3f588a3e22939fb1623e2f759a84c0302011e
andrew <andrew.lyle@tshirtgang.com>
2019-12-17 12:24:22 -0500

    Added Feels like option
    - Added option to enable Feels Like weather.
    - Icon adjusts size based on weather returning
    - Hover over displays current temperature and feels like
    - Fixed issue with int not being properly rounded.

--- weather.py
+++ weather.py
@@ -11,7 +11,11 @@ from PIL import Image, ImageDraw,ImageFont
 city = "{CITY HERE}"
 country = "US" #two digit code
 units = "metric" #testing or imperial
-#api key
+
+useFeelsLike = True; #use feels like
+temperature = 0
+feelslike = 0
+#openweathermap.org API key:
 weatherapi_key = "{API_KEY_HERE}"
 
 #build API URL
@@ -23,6 +27,9 @@ PARAMS = ""
 HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 def checkWeather():
+    global temperature
+    global feelslike
+    #threading.Timer(60.0, checkWeather).start()
     # sending get request and saving the response as response object 
     r = requests.post(url = URL, json = PARAMS, headers = HEADERS) 
 
@@ -30,8 +37,14 @@ def checkWeather():
     data = r.json() 
 
     # extracting latitude, longitude and formatted address  
-    # of the first matching location    
-    return int(data['main']['temp'])
+    # of the first matching location
+    temperature = int(round(data['main']['temp']))
+    feelslike = int(round(data['main']['feels_like']))
+
+    if useFeelsLike == True:
+        return feelslike
+    else:
+        return temperature
 
 #draw icon
 image= "icon.ico"
@@ -39,22 +52,36 @@ n=1
 while True:
     #check weather
     temp = checkWeather()
+
+    #adjust size of font
+    if len(str(temp)) > 3:
+        fontSize = 20
+    elif len(str(temp)) > 2:
+        fontSize = 25
+    elif len(str(temp)) > 1:
+        fontSize = 35
+    else:
+        fontSize = 40
+        
     # create image
     img = Image.new('RGBA', (50, 50), color = (255, 255, 255, 0))  # color background =  white  with transparency
     d = ImageDraw.Draw(img)
 
     #add text to the image
-    font_type  = ImageFont.truetype("arial.ttf", 40)
+    font_type  = ImageFont.truetype("arial.ttf", fontSize)
     d.text((5,5), str(temp)+"°", fill=(255,255,255), font = font_type)
 
     img.save(image)
 
+    #generate text for overlay
+    hover_text = city+" weather\nCurrent Temperature: "+str(temperature)+"°\nFeels like: "+str(feelslike)+"°"
+    
     # display image in systray 
     if n==1:
-        systray = SysTrayIcon(image, city+" weather")
+        systray = SysTrayIcon(image, hover_text)
         systray.start()
     else:
-        systray.update(icon=image)
+        systray.update(icon=image, hover_text=hover_text)
     time.sleep(120)
     n+=1
 systray.shutdown()
